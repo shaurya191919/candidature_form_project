0565659befdb8490d82eb7fa2fc414c3af262f5b
shaurya191919 <shauryajj19@gmail.com>
2023-10-25 16:10:54 +0530

    continue

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const textElement = document.querySelector('#fname');
+    textElement.style.color = 'red';
+});
+
+
+const textt=document.getElementsByClassName('textt')
+
+
+for (let i = 0; i < textt.length; i++) {
+    textt[i].style.color = 'blue';
+}
